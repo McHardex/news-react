b4789b12e494238e779ece0a24237cd4aea2238e
mchardex <mchardex@gmail.com>
2018-11-02 17:59:00 +0100

    completed writers Page mobile responsiveness

--- src/components/Writers.jsx
+++ src/components/Writers.jsx
@@ -21,18 +21,19 @@ export class Writers extends Component {
           <Link to='users'>users</Link>
           <Link to='profile'>profile</Link>
         </header>
-        <div className='nav-head'>Writers</div>
+        <span className='nav-head'>Writers</span>
         {writers.map(writer => {
-          return (<div className='writerBody' key={writer._id}>
-          <div className='writers'>
-            <label className='writer-label'>Name: </label><span>{writer.name}</span>
-          </div>
-          <div className='writers'>
-            <label className='writer-label'>Email: </label><span>{writer.email}</span>
-          </div>
-          <div className='writers'>
-            <label className='writer-label'>Bio: </label><span>{writer.bio}</span>
-          </div>
+          return (
+          <div className='writerBody' key={writer._id}>
+            <div className='writers'>
+              <label className='writer-label'>Name: </label><span>{writer.name}</span>
+            </div>
+            <div className='writers'>
+              <label className='writer-label'>Email: </label><span>{writer.email}</span>
+            </div>
+            <div className='writers'>
+              <label className='writer-label'>Bio: </label><span>{writer.bio}</span>
+            </div>
             <WriterArticle articles={writer.articles} />
           </div>
         )})}
