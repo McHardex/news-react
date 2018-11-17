60e921a264ac3b01d440585dcbc7aa5e9f4e86be
mchardex <mchardex@gmail.com>
2018-11-17 22:58:13 +0100

    Updates

---
+++ precache-manifest.3493f535290c2b3a3ef2dc8907adead5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08cbe5e2f91495199a07",
+    "url": "/news-react/static/js/runtime~main.08cbe5e2.js"
+  },
+  {
+    "revision": "b8ab1fb1a60d2b4d433f",
+    "url": "/news-react/static/js/main.b8ab1fb1.chunk.js"
+  },
+  {
+    "revision": "00a7634a75f4e3608d74",
+    "url": "/news-react/static/js/1.00a7634a.chunk.js"
+  },
+  {
+    "revision": "b8ab1fb1a60d2b4d433f",
+    "url": "/news-react/static/css/main.bae31350.chunk.css"
+  },
+  {
+    "revision": "2b5052829ab4a1b314db488a99f37017",
+    "url": "/news-react/index.html"
+  }
+];
