212305876e2d882abb203107462b52f62852f834
mchardex <mchardex@gmail.com>
2018-11-25 18:51:07 +0100

    added search bar feature

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,12 +1,43 @@
 import React, { Component } from 'react'
 
 class UserList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchValue: '',
+      currentlyDisplayed: this.props.usersList
+    }
+
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onChange = (event) => {
+    let searchContent = event.target.value.toLowerCase()
+    let newlyDisplayed = this.props.usersList.filter(user => {
+      return (
+        user.name.includes(searchContent) ||
+        user.email.includes(searchContent)
+      )
+    })
+    this.setState({ 
+      searchValue: event.target.value,
+      currentlyDisplayed: newlyDisplayed
+    })
+  }
+
   render() {
     return(
       <div>
-        <span className='nav-head'>Users</span>
+        <div className='searchCont'>
+          <span className='nav-head'>Users</span>
+            <div className="search">
+              <div>
+                <input type="search" placeholder="Search . . ." className='search-input' onChange={this.onChange} required />
+              </div>
+            </div>
+        </div>
         { 
-          this.props.usersList.map(user => {
+          this.state.currentlyDisplayed.map(user => {
             return (
               <div className='userBody' key={user._id}>
                 <div className='users'>
